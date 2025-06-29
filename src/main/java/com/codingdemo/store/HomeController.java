d86ea9ae53374701610ae34ee18cd4d570b74c68
Touseef Khan <touseefkhan4pk@gmail.com>
2025-06-29 17:55:22 +0500

    store changes

---
+++ src/main/java/com/codingdemo/store/HomeController.java
@@ -0,0 +1,14 @@
+package com.codingdemo.store;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/contact")
+    public String index(){
+        return "Hello World";
+    }
+}
