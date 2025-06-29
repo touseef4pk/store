f9c266077c90d032075edd063c1cf579d663c189
Touseef Khan <touseefkhan4pk@gmail.com>
2025-06-29 19:22:08 +0500

    Update HomeController.java

--- src/main/java/com/codingdemo/store/HomeController.java
+++ src/main/java/com/codingdemo/store/HomeController.java
@@ -11,7 +11,7 @@ public class HomeController {
     public String index(){
 
         System.out.println("Hello world");
-        return "feature change for PR";
+        return "another made changes";
     }
 
     @RequestMapping("/contact")
